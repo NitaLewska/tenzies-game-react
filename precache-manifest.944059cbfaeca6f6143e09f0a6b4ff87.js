a5209ff2d42ca49c25dc204465b5c3f4a6dd2d18
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2022-11-15 20:43:34 +0300

    Updates

---
+++ precache-manifest.944059cbfaeca6f6143e09f0a6b4ff87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "842e332ad4f72ce9c70b",
+    "url": "/tenzies-game-react/static/css/main.26622027.chunk.css"
+  },
+  {
+    "revision": "842e332ad4f72ce9c70b",
+    "url": "/tenzies-game-react/static/js/main.d6219f3d.chunk.js"
+  },
+  {
+    "revision": "704c69b3bd2767a3b8e0",
+    "url": "/tenzies-game-react/static/js/runtime~main.4188bcf2.js"
+  },
+  {
+    "revision": "8473f0cdd34e452c0be0",
+    "url": "/tenzies-game-react/static/js/2.0b81b788.chunk.js"
+  },
+  {
+    "revision": "ef629e64c60b67db0ecfd97ca3f810fa",
+    "url": "/tenzies-game-react/index.html"
+  }
+];
