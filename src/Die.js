31ac9c867149c0efb2b7b174ae716d8beb83751f
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2022-11-15 21:02:46 +0300

    feat changes numbers into dots
    -add dice-1.0.min.css
    -add dots as a background

--- src/Die.js
+++ src/Die.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Die.css"
+import "./dice-1.0.min.css"
 
 export default function Die(props) {
     return (
-        <button className={'dice' + (props.isHeld ? " held" : "")} onClick={props.holdDice}>{props.value}</button>
+        <button className={'dice' + (props.isHeld ? " held" : "") + (" dice-" + props.value)} onClick={props.holdDice}></button>
     )
 }
